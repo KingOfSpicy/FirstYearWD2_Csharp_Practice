9818a6e8d6844fb571ed6cab3694d58da20b084b
KingOfSpicy <sangowosan@gmail.com>
2022-03-02 11:47:02 +0200

    add workings to folder.

---
+++ WDchewei5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDchewei5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Youngsters");
+            //Console.WriteLine("This week it will be C#.");
+            //Console.WriteLine("Types, Variables,...and Values");
+            //int first = 18;
+            //int second = 6;
+            //int sum = first + second;
+            //Console.WriteLine($"The sum was {sum}!");
+            //int first = 18;
+            //int second = 6;
+            //int sub = first - second;
+            //Console.WriteLine($"The substraction was {sub}!");
+            //int first = 18;
+            //int second = 6;
+            //int divi = first / second;
+            //Console.WriteLine($"The division was {divi}!");
+            //int a = 18;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //Console.WriteLine(d);
+            //int a = 7;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //int e = (a + b) % c;
+            //Console.WriteLine($"quotient: {d}");
+            //Console.WriteLine($"remainder: {e}");
+            //int max = int.MaxValue;
+            //int min = int.MinValue;
+            //Console.WriteLine($"The range of integers is {min} to {max}");
+            //int what = max + 3;
+            //Console.WriteLine($"An example of overflow: {what}");
+            //double a = 19;
+            //double b = 23;
+            //double c = 8;
+            //double d = (a + b) / c;
+            //Console.WriteLine(d);
+            //double max = double.MaxValue;
+            //double min = double.MinValue;
+            //Console.WriteLine($"The range of double is {min} to {max}");
+            //double third = 1.0 / 3.0;
+            //Console.WriteLine(third);
+            //decimal min = decimal.MinValue;
+            //decimal max = decimal.MaxValue;
+            //Console.WriteLine($"The range of the decimal type is {min} to {max}");
+            //double a = 1.0;
+            //double b = 3.0;
+            //Console.WriteLine(a / b);
+
+            //decimal c = 1.0M;
+            //decimal d = 3.0M;
+            //Console.WriteLine(c / d);
+
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+
+        }
+    }
+}
